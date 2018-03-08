13384bd2216fad4225ab2330ebcee2262786cc27
henli070 <henli070@student.liu.se>
2018-03-08 12:26:12 +0100

    Make string literals consistently use single quote

--- Server/src/server.js
+++ Server/src/server.js
@@ -3,7 +3,7 @@ const URL = require('url');
 const PATH = require('path');
 const FS = require('fs');
 
-const HOST_PATH = PATH.join(__dirname, "/../../Client/dist"); // Hardcoded right now
+const HOST_PATH = PATH.join(__dirname, '/../../Client/dist'); // Hardcoded right now
 const PORT = 80;
 
 HTTP.createServer(function(request, response) {
@@ -12,8 +12,8 @@ HTTP.createServer(function(request, response) {
 
   FS.exists(fileName, function(exists) {
     if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
+      response.writeHead(404, {'Content-Type': 'text/plain'});
+      response.write('404 Not Found\n');
       response.end();
       return;
     }
@@ -22,15 +22,15 @@ HTTP.createServer(function(request, response) {
       fileName += '/index.html';
     }
 
-    FS.readFile(fileName, "binary", function(err, file) {
+    FS.readFile(fileName, 'binary', function(err, file) {
       if(err) {
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
+        response.writeHead(500, {'Content-Type': 'text/plain'});
+        response.write(err + '\n');
         response.end();
         return;
       }
       response.writeHead(200);
-      response.write(file, "binary");
+      response.write(file, 'binary');
       response.end();
     });
   });
