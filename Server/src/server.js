bdb879277c405e65d4650663e9c93bc4f4b82ff4
henli070 <henli070@student.liu.se>
2018-03-26 19:23:57 +0200

    Rewrite server to use the express framework

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,37 +1,7 @@
-const HTTP = require('http');
-const URL = require('url');
-const PATH = require('path');
-const FS = require('fs');
+const express = require('express');
 
-const HOST_PATH = PATH.join(__dirname, '/../../Client/dist'); // Hardcoded right now
-const PORT = 80;
+const app = express();
 
-HTTP.createServer(function(request, response) {
-  var uri = URL.parse(request.url).pathname;
-  var fileName = PATH.join(HOST_PATH, uri);
+app.use(express.static(__dirname + '/../../Client/dist'));
 
-  FS.exists(fileName, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {'Content-Type': 'text/plain'});
-      response.write('404 Not Found\n');
-      response.end();
-      return;
-    }
-
-    if (FS.statSync(fileName).isDirectory()) {
-      fileName += '/index.html';
-    }
-
-    FS.readFile(fileName, 'binary', function(err, file) {
-      if(err) {
-        response.writeHead(500, {'Content-Type': 'text/plain'});
-        response.write(err + '\n');
-        response.end();
-        return;
-      }
-      response.writeHead(200);
-      response.write(file, 'binary');
-      response.end();
-    });
-  });
-}).listen(PORT);
+app.listen(80);
