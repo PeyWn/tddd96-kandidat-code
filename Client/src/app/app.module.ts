4a15da2b3c76d4d94f005d8b6b052c3c47e80a12
henli070 <henli070@student.liu.se>
2018-04-01 22:10:49 +0200

    Create application frame

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { MenubarComponent } from './main/menubar/menubar.component';
+import { SidebarComponent } from './main/sidebar/sidebar.component';
+import { ContentComponent } from './main/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    MenubarComponent,
+    SidebarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
