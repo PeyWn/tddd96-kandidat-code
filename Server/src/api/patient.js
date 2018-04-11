07607849feee56f9fe7890862c664b4f963e9242
henli070 <henli070@student.liu.se>
2018-04-11 15:10:06 +0200

    Complete patient api

--- Server/src/api/patient.js
+++ Server/src/api/patient.js
@@ -1,10 +1,12 @@
 const DB = require('./../models');
 module.exports.initAPI = function(APP) {
+  //Get all patients
   APP.get('/patient', function(req, res) {
     DB.Patient.findAll({attributes: {exclude: ['createdAt', 'updatedAt']}}).then(function(patients) {
       res.send(patients);
     });
   });
+  //Add patient
   APP.post('/patient', function(req, res) {
     DB.Patient.create(req.body).then(function(result) {
       res.end(); // Should be 201?
@@ -12,7 +14,7 @@ module.exports.initAPI = function(APP) {
       res.status(500).send(err);
     });
   });
-
+  //Get patient by ssn
   APP.get('/patient/:ssn', function(req, res) {
     DB.Patient.findById(req.params.ssn, {attributes: {exclude: ['createdAt', 'updatedAt']}}).then(function(patient) {
       res.send(patient);
@@ -20,7 +22,23 @@ module.exports.initAPI = function(APP) {
       res.status(500).send(err);
     });
   });
-
+  //Update patient by ssn
+  APP.put('/patient/:ssn', function(req, res) {
+    DB.Patient.update(req.body, {where: {ssn: req.params.ssn}}).then(function(result) {
+      res.end();
+    }).catch(function(err) {
+      res.status(500).send(err);
+    });
+  });
+  //Delete patient by ssn
+  APP.delete('/patient/:ssn', function(req, res) {
+    DB.Patient.destroy({where: {ssn: req.params.ssn}}).then(function(result) {
+      res.end();
+    }).catch(function(err) {
+      res.status(500).send(err);
+    });
+  });
+  //Get decisions for patient by ssn
   APP.get('/patient/:ssn/decisions', function(req, res) {
     DB.Patient.findById(req.params.ssn).then(function(patient) {
       if (patient) {
