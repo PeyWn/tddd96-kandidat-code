3e8e0f34b30b93dff16b2ce3dbf5ac340f1162ae
henli070 <henli070@student.liu.se>
2018-04-11 14:51:26 +0200

    Remove test-api

--- Server/src/api.js
+++ Server/src/api.js
@@ -1,6 +1,8 @@
 const EXPRESS = require('express');
-const PATIENT = require('./api/patient');
 const DB = require('./models');
+
+const PATIENT = require('./api/patient');
+
 DB.sequelize.authenticate().then(function() {
   console.log('Successfully connected to database!');
 }).catch(function(err) {
@@ -13,13 +15,4 @@ module.exports.initAPI = function(APP) {
   APP.use(EXPRESS.urlencoded({extended: true}));
 
   PATIENT.initAPI(APP);
-
-  // One example, TODO Replace with actual
-  APP.get('/api/test', function (req, res) {
-    var result = [
-      { name: 'Test1', age: 50 },
-      { name: 'Test2', age: 37 }
-      ];
-    res.send(result);
-  });
 };
