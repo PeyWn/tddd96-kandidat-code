184394919e80da371ccf9d6a0b5e078b465f1164
henli070 <henli070@student.liu.se>
2018-04-11 14:40:51 +0200

    Add post to /patient and error handling

--- Server/src/api/patient.js
+++ Server/src/api/patient.js
@@ -5,22 +5,35 @@ module.exports.initAPI = function(APP) {
       res.send(patients);
     });
   });
+  APP.post('/patient', function(req, res) {
+    DB.Patient.create(req.body).then(function(result) {
+      res.end(); // Should be 201?
+    }).catch(function(err) {
+      res.status(500).send(err);
+    });
+  });
 
   APP.get('/patient/:ssn', function(req, res) {
     DB.Patient.findById(req.params.ssn, {attributes: {exclude: ['createdAt', 'updatedAt']}}).then(function(patient) {
       res.send(patient);
+    }).catch(function(err) {
+      res.status(500).send(err);
     });
   });
 
   APP.get('/patient/:ssn/decisions', function(req, res) {
     DB.Patient.findById(req.params.ssn).then(function(patient) {
-      if (!patient) {
-        res.end();
-        return;
+      if (patient) {
+        patient.getDecisions({attributes: {exclude: ['createdAt', 'updatedAt']}}).then(function(decisions) {
+          res.send(decisions);
+        }).catch(function(err) {
+          res.status(500).send(err);
+        });
+      } else {
+        res.status(404).send('Patient not found.');
       }
-      patient.getDecisions({attributes: {exclude: ['createdAt', 'updatedAt']}}).then(function(decisions) {
-        res.send(decisions);
-      });
+    }).catch(function(err) {
+      res.status(500).send(err);
     });
   });
 };
