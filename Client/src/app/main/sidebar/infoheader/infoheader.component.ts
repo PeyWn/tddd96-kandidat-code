379d0331173950bca327da87212951f377e09fee
bjohv276 <bjohv276@student.liu.se>
2018-04-12 13:08:45 +0200

    Added new sidebar components.

---
+++ Client/src/app/main/sidebar/infoheader/infoheader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-infoheader',
+  templateUrl: './infoheader.component.html',
+  styleUrls: ['./infoheader.component.css']
+})
+export class InfoheaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
