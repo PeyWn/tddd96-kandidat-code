914784e13db785952d717d9d6d46d8b915d7c172
henli070 <henli070@student.liu.se>
2018-04-12 14:14:07 +0200

    Add seed to link decisions with procedures

--- Server/src/models/decisionprocedure.js
+++ Server/src/models/decisionprocedure.js
@@ -1,7 +1,10 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var DecisionProcedure = sequelize.define('DecisionProcedure', {
-    procedureOrder: DataTypes.INTEGER,
+    procedureOrder: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
     side: DataTypes.STRING
   }, {});
   DecisionProcedure.associate = function(models) {
