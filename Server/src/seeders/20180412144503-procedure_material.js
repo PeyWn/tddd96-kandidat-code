886676bc139cd4b5528cf628d399f02d31c312b8
henli070 <henli070@student.liu.se>
2018-04-13 09:42:40 +0200

    Create seed to link materials and procedures. Fix materials seed

---
+++ Server/src/seeders/20180412144503-procedure_material.js
@@ -0,0 +1,331 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('procedure_material', [{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 1,
+      ProcedureKvåCode: 'JEA00'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 2,
+      ProcedureKvåCode: 'KEC01'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 3,
+      ProcedureKvåCode: 'KEC01'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 4,
+      ProcedureKvåCode: 'KEC01'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 5,
+      ProcedureKvåCode: 'KEC01'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 6,
+      ProcedureKvåCode: 'KEC01'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 7,
+      ProcedureKvåCode: 'KEC01'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 8,
+      ProcedureKvåCode: 'KEC01'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 9,
+      ProcedureKvåCode: 'KEC01'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 10,
+      ProcedureKvåCode: 'LCD00'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 11,
+      ProcedureKvåCode: 'LCD00'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 12,
+      ProcedureKvåCode: 'LCD00'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 13,
+      ProcedureKvåCode: 'LCD00'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 14,
+      ProcedureKvåCode: 'LCD00'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 15,
+      ProcedureKvåCode: 'BAA60'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 16,
+      ProcedureKvåCode: 'BAA60'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 14,
+      ProcedureKvåCode: 'BAA60'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 17,
+      ProcedureKvåCode: 'BAA60'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 18,
+      ProcedureKvåCode: 'KED22'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 14,
+      ProcedureKvåCode: 'KED22'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 19,
+      ProcedureKvåCode: 'KED22'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 20,
+      ProcedureKvåCode: 'JMA10'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 21,
+      ProcedureKvåCode: 'JMA10'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 22,
+      ProcedureKvåCode: 'JMA10'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 23,
+      ProcedureKvåCode: 'JMA10'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 24,
+      ProcedureKvåCode: 'JMA10'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 25,
+      ProcedureKvåCode: 'UJD02'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 26,
+      ProcedureKvåCode: 'UJD02'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 27,
+      ProcedureKvåCode: 'KAC00'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 28,
+      ProcedureKvåCode: 'KAC00'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 29,
+      ProcedureKvåCode: 'KAC00'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 18,
+      ProcedureKvåCode: 'KAC00'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 14,
+      ProcedureKvåCode: 'KAC00'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 30,
+      ProcedureKvåCode: 'KAC00'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 31,
+      ProcedureKvåCode: 'KAC00'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 32,
+      ProcedureKvåCode: 'KAC00'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 33,
+      ProcedureKvåCode: 'KAC00'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 34,
+      ProcedureKvåCode: 'KAC00'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 35,
+      ProcedureKvåCode: 'KAC00'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 36,
+      ProcedureKvåCode: 'JJB30'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 37,
+      ProcedureKvåCode: 'JJB30'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 22,
+      ProcedureKvåCode: 'JJB30'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 38,
+      ProcedureKvåCode: 'JJB30'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 39,
+      ProcedureKvåCode: 'JJB30'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 40,
+      ProcedureKvåCode: 'JJB30'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 14,
+      ProcedureKvåCode: 'JJB30'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 41,
+      ProcedureKvåCode: 'JJB30'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 42,
+      ProcedureKvåCode: 'JJB30'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 43,
+      ProcedureKvåCode: 'JJB30'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 44,
+      ProcedureKvåCode: 'JJB30'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 45,
+      ProcedureKvåCode: 'JJB30'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 40,
+      ProcedureKvåCode: 'JKA20'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 39,
+      ProcedureKvåCode: 'JKA20'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 36,
+      ProcedureKvåCode: 'JKA20'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 22,
+      ProcedureKvåCode: 'JKA20'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 37,
+      ProcedureKvåCode: 'JKA20'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 38,
+      ProcedureKvåCode: 'JKA20'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 46,
+      ProcedureKvåCode: 'JKA20'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 47,
+      ProcedureKvåCode: 'JKA20'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 48,
+      ProcedureKvåCode: 'JKA20'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 14,
+      ProcedureKvåCode: 'JKA20'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 49,
+      ProcedureKvåCode: 'JKA20'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 50,
+      ProcedureKvåCode: 'JKA20'
+    },{
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MaterialId: 51,
+      ProcedureKvåCode: 'JKA20'
+    }]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('procedure_material');
+  }
+};
