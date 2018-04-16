d54c8bee44c5d243a5ccf0d57035997bc6d10c0f
bjohv276 <bjohv276@student.liu.se>
2018-04-16 12:27:41 +0200

    Started work on day-view

--- Client/src/app/main/content/day-view/day-view.component.ts
+++ Client/src/app/main/content/day-view/day-view.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./day-view.component.css']
 })
 export class DayViewComponent implements OnInit {
+  viewDate = new Date();
+  events = [{
+    title: 'TEST',
+    color: {
+      primary: '#e3bc08',
+      secondary: '#fdf1ba'
+    },
+    start: new Date(2018,4,16,15)
+
+  }]
 
   constructor() { }
 
