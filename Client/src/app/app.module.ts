a71e1e1e5609f57200775f13e28c8d2b8b7ec999
henli070 <henli070@student.liu.se>
2018-04-18 13:38:30 +0200

    Added missing provider for booking service

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -11,6 +11,7 @@ import {PatientService} from './http-api/patient/patient.service';
 import {MaterialService} from './http-api/material/material.service';
 import {Icd10Service} from './http-api/icd10/icd10.service';
 import {ClinicService} from './http-api/clinic/clinic.service';
+import {BookingService} from './http-api/booking/booking.service';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import {ClinicService} from './http-api/clinic/clinic.service';
     PatientService,
     MaterialService,
     Icd10Service,
-    ClinicService
+    ClinicService,
+    BookingService
   ],
   bootstrap: [AppComponent]
 })
