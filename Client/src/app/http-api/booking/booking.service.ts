46e047cca8534a900ba138e526ffa88ccc44dbc5
henli070 <henli070@student.liu.se>
2018-04-18 13:32:38 +0200

    Finished get, post, delete for bookings

---
+++ Client/src/app/http-api/booking/booking.service.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {Booking} from './Booking';
+import { HttpHeaders } from '@angular/common/http';
+import {StaffBooking} from '../staff/StaffBooking';
+import {MaterialBooking} from '../material/MaterialBooking';
+import {RoomBooking} from '../room/RoomBooking';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'my-auth-token'
+  })
+};
+
+@Injectable()
+export class BookingService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllBookings(): Observable<Booking[]> {
+    return this.httpClient.get<Booking[]>('/booking');
+  }
+
+  getBooking(id: number): Observable<Booking> {
+    return this.httpClient.get<Booking>('/booking/' + id);
+  }
+
+  createBooking(decisionId: number, preliminary: boolean): Observable<Booking> {
+    return this.httpClient.post<Booking>('/booking', {preliminary: preliminary, DecisionId: decisionId}, httpOptions);
+  }
+
+  updateBooking(bookingId: number, preliminary: boolean): Observable<Object> {
+    return this.httpClient.put('/booking/' + bookingId, {preliminary: preliminary}, httpOptions);
+  }
+
+  deleteBooking(bookingId: number): Observable<Object> {
+    return this.httpClient.delete('/booking/' + bookingId, httpOptions);
+  }
+
+  getBookedStaff(bookingId: number): Observable<StaffBooking[]> {
+    return this.httpClient.get<StaffBooking[]>('/booking/' + bookingId + '/staff');
+  }
+
+  addStaffToBooking(bookingId: number, staffId: number, startDate: Date, endDate: Date): Observable<Object> {
+    return this.httpClient.post('/booking/' + bookingId + '/staff', {staffId: staffId, startDate: startDate, endDate: endDate},
+      httpOptions);
+  }
+
+  removeStaffFromBooking(bookingId: number, staffId: number): Observable<Object> {
+    return this.httpClient.delete('/booking/' + bookingId + '/staff/' + staffId, httpOptions);
+  }
+
+  getBookedMaterial(bookingId: number): Observable<MaterialBooking[]> {
+    return this.httpClient.get<MaterialBooking[]>('/booking/' + bookingId + 'material');
+  }
+
+  addMaterialToBooking(bookingId: number, materialId: number, startDate: Date, endDate: Date): Observable<Object> {
+    return this.httpClient.post('/booking/' + bookingId + '/material', {materialId: materialId, startDate: startDate, endDate: endDate},
+      httpOptions);
+  }
+
+  removeMaterialFromBooking(bookingId: number, materialId: number): Observable<Object> {
+    return this.httpClient.delete('/booking/' + bookingId + '/material/' + materialId);
+  }
+
+  getBookedRooms(bookingId: number): Observable<RoomBooking[]> {
+    return this.httpClient.get<RoomBooking[]>('/booking/' + bookingId + '/room');
+  }
+
+  addRoomToBooking(bookingId: number, roomId: number, startDate: Date, endDate: Date): Observable<Object> {
+    return this.httpClient.post('/booking/' + bookingId + '/room', {roomId: roomId, startDate: startDate, endDate: endDate},
+      httpOptions);
+  }
+
+  removeRoomFromBooking(bookingId: number, roomId: number): Observable<Object> {
+    return this.httpClient.delete('/booking/' + bookingId + '/room/' + roomId);
+  }
+}
