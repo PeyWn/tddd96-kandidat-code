d9f7c12cad0e226285309a313d95ff5352f104ce
henli070 <henli070@student.liu.se>
2018-04-26 14:05:17 +0200

    Began fixing inbox

--- Client/src/app/main/get-patients.service.ts
+++ Client/src/app/main/get-patients.service.ts
@@ -23,24 +23,18 @@ export class GetPatientsService {
       for (let i = 0; i < decisions.length; i++) {
         this.patientService.getPatient(decisions[i].PatientSsn).subscribe((patient: PatientResponse) => {
           this.decisionService.getProceduresForDecision(decisions[i].id).subscribe((procedures: ProcedureResponse[]) => {
-            let mainProcedure: ProcedureResponse;
+            let sortedProcedures: ProcedureResponse[];
             for (let j = 0; j < procedures.length; j++) {
-              if (procedures[j].DecisionProcedure.procedureOrder === 1) {
-                mainProcedure = procedures[j];
-              }
+              sortedProcedures[procedures[j].DecisionProcedure.procedureOrder - 1] = procedures[j];
             }
             let newPatient: Patient = new Patient(
               patient.firstName + ' ' + patient.lastName,
               patient.ssn,
-              mainProcedure.description,
               decisions[i].ICD10Code,
               decisions[i].urgent,
               new Date(decisions[i].latestDate),
-              mainProcedure.kvåCode,
-              mainProcedure.preparationTime,
-              mainProcedure.operationTime,
-              mainProcedure.downTime,
-              false);
+              false,
+              sortedProcedures);
             this.patients.push(newPatient);
             this.fetchedPatient.emit();
           });
--- Client/src/app/main/sidebar/planning/infoheader/Patient.ts
+++ Client/src/app/main/sidebar/planning/infoheader/Patient.ts
@@ -1,39 +1,28 @@
+import {ProcedureResponse} from '../../../../http-api/procedure/ProcedureResponse';
+
 export class Patient {
   Namn: string;
   Personnummer: string;
-  Operationstyp: string;
   ICD10: string;
   Bradskandegrad: boolean;
   Tid: Date;
-  KVA: string;
-  Forberedelsetid: number;
-  Operationtid: number;
-  Avvecklingstid: number;
   Bokad: boolean;
-
+  procedures: ProcedureResponse[];
 
   constructor(Namn: string,
               Personnummer: string,
-              Operationstyp: string,
               ICD10: string,
               Bradskandegrad: boolean,
               Tid: Date,
-              KVA: string,
-              forbtid: number,
-              optid: number,
-              avtid: number,
-              bokad: boolean) {
+              bokad: boolean,
+              procedures: ProcedureResponse[]) {
 
     this.Namn = Namn;
     this.Personnummer = Personnummer;
-    this.Operationstyp = Operationstyp;
     this.ICD10 = ICD10;
     this.Bradskandegrad = Bradskandegrad;
     this.Tid = Tid;
-    this.KVA = KVA;
-    this.Forberedelsetid = forbtid;
-    this.Operationtid = optid;
-    this.Avvecklingstid = avtid;
     this.Bokad = bokad;
+    this.procedures = procedures;
   }
 }
