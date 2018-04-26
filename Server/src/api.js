a052b5bbe9fb89be9f345cace1cceac19d8ac69b
henli070 <henli070@student.liu.se>
2018-04-26 09:04:56 +0200

    Restructure

--- Server/src/api.js
+++ Server/src/api.js
@@ -1,6 +1,4 @@
 const EXPRESS = require('express');
-const PASSPORT = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 const DB = require('./models');
 
 const PATIENT = require('./api/patient');
@@ -12,6 +10,7 @@ const STAFF = require('./api/staff');
 const ROOM = require('./api/room');
 const PROCEDURE = require('./api/procedure');
 const ICD10 = require('./api/icd10');
+const LOGIN = require('./api/login');
 
 DB.sequelize.authenticate().then(function() {
   console.log('Successfully connected to database!');
@@ -19,63 +18,14 @@ DB.sequelize.authenticate().then(function() {
   console.error('Unable to connect to database:', err);
 });
 
-PASSPORT.use(new LocalStrategy(
-  function(username, password, done) {
-    DB.User.findOne({where: {username: username}}).then(function(user) {
-      if (!user) {
-        return done(null, false);
-      }
-      if (user.password !== password) {
-        return done(null, false);
-      }
-      return done(null, user);
-    });
-  }
-));
-
-PASSPORT.serializeUser(function(user, cb) {
-  cb(null, user.id);
-});
-
-PASSPORT.deserializeUser(function(id, cb) {
-  DB.User.findById(id).then(function(user) {
-    cb(null, user);
-  }).catch(function(err) {
-    cb(err);
-  });
-});
-
 module.exports.initAPI = function(APP) {
   APP.use(EXPRESS.json());
   APP.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
   APP.use(EXPRESS.urlencoded({extended: true}));
 
-  APP.use(PASSPORT.initialize());
-  APP.use(PASSPORT.session());
+  LOGIN.initAPI(APP);
 
-  APP.get('/api/login', function(req, res) {
-    res.send(req.isAuthenticated());
-  });
-  APP.post('/api/login', function(req, res, next) {
-    PASSPORT.authenticate('local', function(err, user, info) {
-      if (err) {
-        return next(err);
-      }
-      if (!user) {
-        return res.send({ success : false, message : 'Incorrect login details' });
-      }
-      req.login(user, loginErr => {
-        if (loginErr) {
-          return next(loginErr);
-        }
-        return res.send({ success : true, message : 'Successful login' });
-      });
-    })(req, res, next);
-  });
-  APP.delete('/api/login', function(req, res) {
-    req.logout();
-    res.end();
-  });
+  // Every api after this will require authentication
   APP.use(function(req, res, next) {
     if (!req.isAuthenticated()) {
       res.status(401).end();
---
+++ Server/src/api/login.js
@@ -0,0 +1,60 @@
+const DB = require('./../models');
+const PASSPORT = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+module.exports.initAPI = function(APP) {
+  PASSPORT.use(new LocalStrategy(
+    function(username, password, done) {
+      DB.User.findOne({where: {username: username}}).then(function(user) {
+        if (!user) {
+          return done(null, false);
+        }
+        if (user.password !== password) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+    }
+  ));
+
+  PASSPORT.serializeUser(function(user, cb) {
+    cb(null, user.id);
+  });
+
+  PASSPORT.deserializeUser(function(id, cb) {
+    DB.User.findById(id).then(function(user) {
+      cb(null, user);
+    }).catch(function(err) {
+      cb(err);
+    });
+  });
+
+  APP.use(PASSPORT.initialize());
+  APP.use(PASSPORT.session());
+
+  APP.get('/api/login', function(req, res) {
+    res.send(req.isAuthenticated());
+  });
+
+  APP.post('/api/login', function(req, res, next) {
+    PASSPORT.authenticate('local', function(err, user, info) {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return res.send({ success : false, message : 'Incorrect login details' });
+      }
+      req.login(user, loginErr => {
+        if (loginErr) {
+          return next(loginErr);
+        }
+        return res.send({ success : true, message : 'Successful login' });
+      });
+    })(req, res, next);
+  });
+
+  APP.delete('/api/login', function(req, res) {
+    req.logout();
+    res.end();
+  });
+};
