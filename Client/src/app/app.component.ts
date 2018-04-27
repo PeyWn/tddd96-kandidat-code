36764c5df86b7d854b3bf6e6fb87ca290a4a548f
torut235 <torut235@student.liu.se>
2018-04-27 15:51:31 +0200

    wierd error with app.component.ts

---
+++ Client/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {LoginService} from './login/login.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor(private loginService: LoginService) { }
+}
