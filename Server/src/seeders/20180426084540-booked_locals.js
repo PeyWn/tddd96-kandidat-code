485d68650dae81b59055d327d37af3eb6c06e283
Christoffer <chsnot@gmail.com>
2018-04-27 13:54:58 +0200

    add one more room booking

--- Server/src/seeders/20180426084540-booked_locals.js
+++ Server/src/seeders/20180426084540-booked_locals.js
@@ -7,6 +7,11 @@ module.exports = {
         start_time: new Date('April 30, 2018 08:00:00'),
         end_time: new Date('April 30, 2018 10:00:00'),
         LocalId: 1
+      }, {
+        BookingId: 1,
+        start_time: new Date('April 30, 2018 09:00:00'),
+        end_time: new Date('April 30, 2018 11:00:00'),
+        LocalId: 3
       }]);
   },
 
