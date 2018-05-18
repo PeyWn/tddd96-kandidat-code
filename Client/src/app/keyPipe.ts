5be48f347a35fe82f32c93b3aaa889c784415993
Christoffer <chsnot@gmail.com>
2018-05-18 11:18:08 +0200

    rewrite keypipe to allow for key and value to be usede

--- Client/src/app/keyPipe.ts
+++ Client/src/app/keyPipe.ts
@@ -8,7 +8,7 @@ export class KeysPipe implements PipeTransform {
   transform(value, args:string[]) : any {
     let keys = [];
     for (let key in value) {
-      keys.push(key);
+      keys.push({key: key, value: value[key]});
     }
     return keys;
   }
