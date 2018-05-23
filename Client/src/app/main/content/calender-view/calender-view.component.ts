3f4a1bd4e4de43e9e836e9a59dcaeb07ea20e3ff
sigge <marpe902@student.liu.se>
2018-05-23 10:01:20 +0200

    MOre info in decison

--- Client/src/app/main/content/calender-view/calender-view.component.ts
+++ Client/src/app/main/content/calender-view/calender-view.component.ts
@@ -325,7 +325,7 @@ export class CalenderViewComponent implements OnInit {
           this.decisionService.getDecision(bookings[i].DecisionId).subscribe((decision: DecisionResponse) => {
             this.roomEvents[room.name].events.push({start: new Date(bookings[i].Booked_local.start_time),
               end: new Date(bookings[i].Booked_local.end_time),
-              title: decision.PatientSsn,
+              title: decision.PatientSsn +'<br/>' + this.currentPatient.Namn + '<br/>' + decision.Staff.firstname + ' ' + decision.Staff.lastname + '<br/>' + this.currentPatient.procedures[0].kvåCode,
               color: colors.blue,
               actions: this.actions});
           });
